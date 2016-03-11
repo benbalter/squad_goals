ef46d63d803d27940026b6606fa67d296890469a
Ben Balter <ben.balter@github.com>
2016-03-11 14:17:17 -0500

    remove debug info

--- lib/squad_goals/helpers.rb
+++ lib/squad_goals/helpers.rb
@@ -2,12 +2,9 @@ module SquadGoals
   module Helpers
     # Call octokit, using memcached response, when available
     def client_call(method, *args)
-      puts "METHOD: #{method}"
-      puts "ARGS: #{args.join(', ')}"
       key = cache_key(method, args)
       cached = dalli.get(key)
       return cached if cached
-      puts 'CACHE MISS'
       response = client.send(method, *args)
       dalli.set(key, response)
       response
